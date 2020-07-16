3feb66ba766bcd904f53c2d330fcc2f5345551f3
Dhananjana94 <47080621+Dhananjana94@users.noreply.github.com>
2020-07-16 13:25:53 +0530

    Add enemy scripts

---
+++ HomeDefence/Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	float speed = 10f;
+
+	private Transform target;
+	private int wavepointIndex = 0; // enemy go through 0th index wavePoint to 11th wavePoint index
+
+	void Start(){
+	
+		target = WayPoints.points[0];
+	}
+
+	void Update(){
+	
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance (transform.position, target.position) <= 0.2f) {
+		
+			GetNextWayPoint();
+		}
+	}
+
+	void GetNextWayPoint(){
+	
+		if (wavepointIndex >= WayPoints.points.Length - 1) {
+		
+			Destroy (gameObject); // when end enemy reach to the end enemy will bw destroied 	
+			return;
+		}
+
+		wavepointIndex++;
+		target = WayPoints.points[wavepointIndex];
+	}
+}
